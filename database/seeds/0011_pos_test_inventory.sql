d9977d0435845a709703c7119700d8e48309b912
DanhTrannn <tranthanhdanhktvn2005d@gmail.com>
2026-09-15 22:18:46 +0700

    seed(pos): update store IDs to match actual data (7-12)

--- database/seeds/0011_pos_test_inventory.sql
+++ database/seeds/0011_pos_test_inventory.sql
@@ -1,22 +1,22 @@
 -- Tồn kho test cho POS
 
--- Store HCM (store_id = 1)
+-- Store Sài Gòn (store_id = 7)
 INSERT INTO store_inventory (store_id, variant_id, on_hand, opening_on_hand, version)
-SELECT 1, variant_id, 10, 10, 1
+SELECT 7, variant_id, 10, 10, 1
 FROM product_variants
 WHERE is_active = 1
 ON DUPLICATE KEY UPDATE on_hand = VALUES(on_hand);
 
--- Store HN (store_id = 2)
+-- Store Thủ Đức (store_id = 8)
 INSERT INTO store_inventory (store_id, variant_id, on_hand, opening_on_hand, version)
-SELECT 2, variant_id, 8, 8, 1
+SELECT 8, variant_id, 8, 8, 1
 FROM product_variants
 WHERE is_active = 1
 ON DUPLICATE KEY UPDATE on_hand = VALUES(on_hand);
 
--- Store DN (store_id = 3)
+-- Store Hoàn Kiếm (store_id = 9)
 INSERT INTO store_inventory (store_id, variant_id, on_hand, opening_on_hand, version)
-SELECT 3, variant_id, 5, 5, 1
+SELECT 9, variant_id, 5, 5, 1
 FROM product_variants
 WHERE is_active = 1
 ON DUPLICATE KEY UPDATE on_hand = VALUES(on_hand);
